01d8b897244a9e8cd4e2ba1f8133d34831675c9d
Seoyeon Kim <seoyeonkim815@gmail.com>
2021-08-16 16:12:45 +0900

    [Update] Logic to handle the creation of a new list

--- server/controllers/list/list.js
+++ server/controllers/list/list.js
@@ -1,26 +1,14 @@
-const fs = require('fs').promises
 const uuid = require('uuid').v4
+const { data, setData } = require('../../data/data.js')
 
 module.exports = async (req, res) => {
   const newList = req.body
+
   const id = uuid()
+  newList.id = id
+
+  const newData = [...data, newList]
+  setData(newData)
+  
   res.status(201).send({id: id})
-
-  // data.json파일에 변경 사항을 저장하는 로직입니다
-  // const previousData = await fs.readFile('data/data.json', 'utf8', (err, data) => {
-  //   if (err) {
-  //     console.log(err)
-  //   } else {
-  //     return data
-  //   }
-  // })
-  // .then(data => JSON.parse(data))
-
-  // const newData = [...previousData, newList]
-
-  // await fs.writeFile('data/data.json', JSON.stringify(newData, null, 2))
-  // .then(() => {
-  //   res.status(201).send({message: "A new list has been successfully created."})
-  // })
-
 }
