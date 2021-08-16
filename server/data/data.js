70cf8afaf0e15997f2a3923199bc9c953d7ba7cb
Seoyeon Kim <seoyeonkim815@gmail.com>
2021-08-16 16:15:26 +0900

    [Edit] Changed the file format to JS

---
+++ server/data/data.js
@@ -0,0 +1,43 @@
+module.exports = {
+  data: [
+    {
+      id: "111",
+      name: "예제 리스트1",
+      cards: [
+        {
+          id: "123",
+          content: "카드1"
+        },
+        {
+          id: "456",
+          content: "카드2"
+        },
+        {
+          id: "789",
+          content: "카드3"
+        }
+      ]
+    },
+    {
+      id: "222",
+      name: "예제 리스트2",
+      cards: [
+        {
+          id: "321",
+          content: "카드1"
+        },
+        {
+          id: "654",
+          content: "카드2"
+        },
+        {
+          id: "987",
+          content: "카드3"
+        }
+      ]
+    }
+  ],
+  setData: ( newData ) => {
+    data = newData
+  }
+}
