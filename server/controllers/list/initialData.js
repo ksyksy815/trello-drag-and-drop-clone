3229fa19ee1611d0aa4bfb3ab0a92263e882816e
Seoyeon Kim <seoyeonkim815@gmail.com>
2021-08-16 16:13:33 +0900

    [Update] Logic to respond to the initial data request

--- server/controllers/list/initialData.js
+++ server/controllers/list/initialData.js
@@ -1,15 +1,7 @@
-const fs = require('fs').promises
+const initialData = require('../../data/data.js')
 
 module.exports = async (req, res) => {
-  const initialData = await fs.readFile('data/data.json', 'utf8', (err, data) => {
-    if (err) {
-      console.log(err)
-    } else {
-      return data
-    }
-  })
-  
   res.status(200).send({
-    initialData
+    initialData: initialData.data
   })
 }
