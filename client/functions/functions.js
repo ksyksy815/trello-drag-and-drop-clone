4cc9b665c3ba84c9b3cd59f4bc15c296084b88de
Seoyeon Kim <seoyeonkim815@gmail.com>
2021-08-16 23:03:07 +0900

    [Update] Added new loginc to update global states. Added two new functions that deal with update requests to the server on drop

--- client/functions/functions.js
+++ client/functions/functions.js
@@ -8,8 +8,9 @@ import {
 import { 
   requestToCreateNewList,
   requestToMakeNewCard,
-  requestToRelocateCard
+  requestToUpdateOrder
 } from './requests.js'
+import { listData, setListData, dragData, setDragData } from '../globalStates.js'
 
 export const makeListsWithData = (data) => {
   data.forEach(list => {
@@ -27,14 +28,18 @@ export const makeNewList = async (e) => {
   e.preventDefault()
 
   const id = await requestToCreateNewList(e.target[0].value)
-  const list = {
+  const newList = {
     id,
     name: e.target[0].value,
     cards: []
   }
 
+  const newListData = [...listData, newList]
+  setListData(newListData)
+
   const mainListWrapper = document.querySelector('#main-list-wrapper')
-  generateNewList( mainListWrapper, list)
+  generateNewList( mainListWrapper, newList)
+
   e.target[0].value = ''
 }
 
@@ -49,13 +54,26 @@ export const handleClickBtnToAddCard = (e) => {
 
 export const handleNewCardSubmission = async (e) => {
   e.preventDefault()
-  let content = e.target[0].value
+
+  const content = e.target[0].value
   const listId = e.target.parentElement.parentElement.children[1].id
-  
-  const id = await requestToMakeNewCard( listId, content )
-  
+  const id = await requestToMakeNewCard( listId, content )  
   const parent = e.target.parentElement.previousElementSibling
+
+  const newListData = listData.map( list => {
+    if (list.id === parent.id) {
+      return {
+        ...list,
+        cards: [...list.cards, { id, content }]
+      }
+    }
+
+    return list
+  })
+
+  setListData(newListData)
   generateOneListItem( parent, content, id )
+
   e.target[0].value = ''
 }
 
@@ -80,10 +98,25 @@ export const closeFormToAddNewCard = (parent, grandParent) => {
 export const addDragStartAndDragEndListener = (draggable) => {
   draggable.addEventListener('dragstart', () => {
     draggable.classList.add('dragging')
+
+    setDragData({
+      ...dragData,
+      draggingId: draggable.id,
+      draggingContent: draggable.textContent,
+      from: draggable.parentElement.id
+    })
   })
 
   draggable.addEventListener('dragend', () => {
     draggable.classList.remove('dragging')
+
+    setDragData({
+      draggingId: null,
+      drraggingContent: null,
+      hoveredEl: null,
+      from: null,
+      to: null
+    })
   })
 }
 
@@ -94,10 +127,14 @@ export const addDragOverListener = (list) => {
     const hoveredElement = getHoveredElement(list, e.clientY)
     const item = document.querySelector('.dragging')
 
+    setDragData({
+      ...dragData,
+      hoveredEl: hoveredElement ? hoveredElement.id : null
+    })
+
     if (hoveredElement === null) {
       list.appendChild(item)
     } else {
-      requestToRelocateCard(item, list.id)
       list.insertBefore(item, hoveredElement)
     }
   })
@@ -116,4 +153,79 @@ const getHoveredElement = (list, y) => {
       return closest
     }
   }, { offset: Number.NEGATIVE_INFINITY }).element
+}
+
+export const addDropListener = (designatedList) => {
+  designatedList.addEventListener('drop', (e) => {
+    // 다른 리스트간 이동일 경우
+    if (dragData.from !== designatedList.id) {
+      const listToSubtract = listData.filter(list => list.id === dragData.from)[0].cards
+      const listToAdd = listData.filter(list => list.id === designatedList.id)[0].cards
+
+      const subtractedList = makeListWithNewOrder(listToSubtract)
+      const addedList = makeListWithNewOrder(listToAdd)
+
+      setListData(
+        listData.map( list => {
+          if (list.id === dragData.from) {
+            return {...list, cards: subtractedList}
+          } else if (list.id === designatedList.id) {
+            return {...list, cards: addedList}
+          } else {
+            return list
+          }
+        })
+      )
+
+      requestToUpdateOrder( subtractedList, dragData.from )
+      requestToUpdateOrder( addedList, designatedList.id )
+
+    // 같은 리스트 내 이동인 경우
+    } else {
+      const currentCards = listData.filter(list => list.id === designatedList.id)[0].cards
+      const newCards = makeListWithNewOrder(currentCards)
+      
+      setListData(
+        listData.map( list => {
+          if (list.id === designatedList.id) {
+            return {...list, cards: newCards}
+          } else {
+            return list
+          }
+        })
+      )
+
+      requestToUpdateOrder( newCards, designatedList.id )
+    }
+  })
+}
+
+const makeListWithNewOrder = ( originalList ) => {
+  const card = {
+    id: dragData.draggingId,
+    content: dragData.draggingContent
+  }
+  
+  let { list, hoveredIndex } = originalList.reduce((acc, cur, i, arr) => {
+    if (cur.id === card.id) {
+      let list = arr.slice()
+      list.splice(i, 1)
+
+      return { ...acc, list: list}
+
+    } else if (cur.id === dragData.hoveredEl) {
+      return { ...acc, hoveredIndex: i }
+
+    } else { return acc }
+  }, {})
+
+  if (list === undefined) list = originalList.slice()
+
+  return (
+    [
+      ...list.slice(0, hoveredIndex), 
+      card, 
+      ...list.slice(hoveredIndex)
+    ]
+  )
 }
